cca512e0b429dfd8d18e636f5f0c58b6a461092a
Alvarodevs <alvaro.develops@gmail.com>
2020-12-10 11:57:50 +0000

    First commit

--- exercises/07-Arrow-Functions/app.js
+++ exercises/07-Arrow-Functions/app.js
@@ -1,4 +1,10 @@
+const rapid = (str) => {
 
+    for (var i in str) {
+        i = str.toUpperCase();
+        return i.replace(/[aeiou]/ig,'');
+    }
+}
    // From this line up Do not change code below
    let str = "John";
    console.log(rapid(str));
